78bb817ebd49bd3171b1a9158c062f1be6c8dadc
ChristineCPham <christinepham44@gmail.com>
2022-06-08 11:46:14 -0700

    Add sketch pad & reset option

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-// Create 16 divs
+// Create divs & out into grid
+const root = document.querySelector(':root');
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
+let divSideNumber = 6;
+let totalDivs = Math.pow(divSideNumber, 2);
+root.style.setProperty('--divNumber', divSideNumber);
+
+for (let i = 0; i < totalDivs; i++) {
     const div = document.createElement('div');
+    div.classList.add('sketchDiv');
     container.appendChild(div);
 }
 
+// Add color sketching properties
+const divs = document.querySelectorAll('.sketchDiv');
+divs.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+        div.style['background-color'] = 'pink';
+    });
+});
 
+//Change the number of divs
+
+
+//Reset the sketch pad
+const reset = document.querySelector('#reset');
+divs.forEach((div) => {
+    reset.addEventListener('click', () => {
+        div.style['background-color'] = 'white';
+    });
+});
+    
 
