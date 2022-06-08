3c1793822c10bfee06d3ad520f10d3c79ce0a013
ChristineCPham <christinepham44@gmail.com>
2022-06-08 12:31:12 -0700

    Add change # of divs feature
    - Put code in functions
    - Add feature to change # of divs in pad by button

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-// Create divs & out into grid
 const root = document.querySelector(':root');
 const container = document.querySelector('#container');
+let divSideNumber = 16;
+let totalDivs;
+playSketch();
 
-let divSideNumber = 6;
-let totalDivs = Math.pow(divSideNumber, 2);
-root.style.setProperty('--divNumber', divSideNumber);
+//Change the number of divs
+const setDivNumber = document.querySelector('#setDivNumber');
+setDivNumber.addEventListener('click', () => {
+    newGrid();
+});
 
-for (let i = 0; i < totalDivs; i++) {
-    const div = document.createElement('div');
-    div.classList.add('sketchDiv');
-    container.appendChild(div);
+//Make new grid
+function newGrid() {
+    deleteGrid();
+    divSideNumber = prompt('How many squares would you like on each side?');
+    playSketch();
+}
+
+// Create divs & puts into grid
+function createGrid() {
+    totalDivs = Math.pow(divSideNumber, 2);
+
+    root.style.setProperty('--divNumber',   divSideNumber);
+
+    for (let i = 0; i < totalDivs; i++) {
+        const div = document.createElement('div');
+        div.classList.add('sketchDiv');
+        container.appendChild(div);
+    }
+}
+
+function deleteGrid() {
+    for (let i = 0; i < totalDivs; i++) {
+        const sketchPad = document.querySelector('.sketchDiv');
+        container.removeChild(sketchPad);
+    }
 }
 
 // Add color sketching properties
-const divs = document.querySelectorAll('.sketchDiv');
-divs.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-        div.style['background-color'] = 'pink';
+function sketch() {
+    const divs = document.querySelectorAll('.sketchDiv');
+    divs.forEach((div) => {
+       div.addEventListener('mouseenter', () => {
+           div.style['background-color'] = 'pink';
+       });
     });
-});
-
-//Change the number of divs
-
+}
 
 //Reset the sketch pad
-const reset = document.querySelector('#reset');
-divs.forEach((div) => {
-    reset.addEventListener('click', () => {
-        div.style['background-color'] = 'white';
+function reset() {
+    const divs = document.querySelectorAll('.sketchDiv');
+    const reset = document.querySelector('#reset');
+    divs.forEach((div) => {
+        reset.addEventListener('click', () => {
+            div.style['background-color'] = 'white';
+        });
     });
-});
+}
+
+function playSketch() {
+    createGrid();
+    sketch();
+    reset();
+}
+
+
     
 
